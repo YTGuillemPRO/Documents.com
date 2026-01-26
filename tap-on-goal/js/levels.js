d5c087cd9026af402f2b064651493eb13f5d3685
YTGuillemPRO <guillevarelacors@gmail.com>
2026-01-26 17:20:20 +0100

    Update levels.js

--- tap-on-goal/js/levels.js
+++ tap-on-goal/js/levels.js
@@ -1,1 +1,9 @@
+// Cada nivel tiene: longitud de camino y número de obstáculos
+const levels = [
+    {length: 10, obstacles: 3},
+    {length: 12, obstacles: 5},
+    {length: 15, obstacles: 7},
+    {length: 18, obstacles: 10},
+    {length: 20, obstacles: 13},
+];
 
