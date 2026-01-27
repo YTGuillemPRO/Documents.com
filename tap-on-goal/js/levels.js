20427755c51fe389f93aec4c408aeee4ca49005c
YTGuillemPRO <guillevarelacors@gmail.com>
2026-01-27 10:34:04 +0100

    Update levels.js

--- tap-on-goal/js/levels.js
+++ tap-on-goal/js/levels.js
@@ -1,9 +1,10 @@
 const levels = [];
 for (let i = 0; i < 50; i++) {
-    levels.push({
-        length: 15 + (i * 2),      // Más largo cada vez
-        obstacles: 10 + (i * 2),   // Más bloques amarillos
-        speed: 7 + (i * 0.1),      // Un poco más rápido
-        reward: 50 + (i * 10)      // Más monedas por nivel
-    });
+  levels.push({
+    length: 15 + i * 2,
+    obstacles: 6 + i * 2,
+    speed: 6 + i * 0.15,
+    reward: 50 + i * 10,
+    goalkeeperSpeed: 2 + i * 0.05
+  });
 }
