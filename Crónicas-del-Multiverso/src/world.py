6b085888775ad886791a6bbeac983515b873a918
YTGuillemPRO <guillevarelacors@gmail.com>
2026-01-27 19:55:37 +0100

    Update world.py

--- Crónicas-del-Multiverso/src/world.py
+++ Crónicas-del-Multiverso/src/world.py
@@ -1,1 +1,10 @@
+import pygame
 
+class World:
+    def __init__(self):
+        self.width = 1600
+        self.height = 1200
+        self.color = (50, 50, 100)  # Fondo azul oscuro
+
+    def draw(self, screen):
+        screen.fill(self.color)
