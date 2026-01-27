0c35b5c556b8ca9800f9110af706b4ffbb098069
YTGuillemPRO <guillevarelacors@gmail.com>
2026-01-27 20:28:30 +0100

    Update player.py

--- Crónicas-del-Multiverso/src/player.py
+++ Crónicas-del-Multiverso/src/player.py
@@ -6,22 +6,28 @@ class Player:
         self.y = y
         self.width = 32
         self.height = 32
-        self.color = (0, 255, 0)
         self.speed = 4
+        
+        # Intentamos cargar la imagen; si falla, usamos un cuadrado verde
+        try:
+            self.image = pygame.image.load("assets/sprites/jugador.png").convert_alpha()
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        except:
+            self.image = None 
+            self.color = (0, 255, 0)
 
     def update(self, keys, world):
-        if keys[pygame.K_w]:
-            self.y -= self.speed
-        if keys[pygame.K_s]:
-            self.y += self.speed
-        if keys[pygame.K_a]:
-            self.x -= self.speed
-        if keys[pygame.K_d]:
-            self.x += self.speed
-
-        # Colisiones simples con el mundo
+        if keys[pygame.K_w]: self.y -= self.speed
+        if keys[pygame.K_s]: self.y += self.speed
+        if keys[pygame.K_a]: self.x -= self.speed
+        if keys[pygame.K_d]: self.x += self.speed
+        
+        # Colisiones con el tamaño del mundo
         self.x = max(0, min(world.width - self.width, self.x))
         self.y = max(0, min(world.height - self.height, self.y))
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        if self.image:
+            screen.blit(self.image, (self.x, self.y))
+        else:
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
